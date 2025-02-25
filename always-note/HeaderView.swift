9942f5e0af5260a331c03e5a1f0bda1263a8d879
AvansIvar <igm.schreutelkamp@student.avans.nl>
2025-02-25 14:01:45 +0100

    Enhance: different screen sizes

--- always-note/HeaderView.swift
+++ always-note/HeaderView.swift
@@ -10,16 +10,28 @@ import SwiftUI
 struct HeaderView: View {
     var decreaseFontSize: () -> Void
     var increaseFontSize: () -> Void
+    @Environment(\.verticalSizeClass) var verticalSizeClass
+    @Environment(\.horizontalSizeClass) var horizontalSizeClass
 
     var body: some View {
-        VStack {
-            TitleView()
+        if horizontalSizeClass == .compact {
+            VStack {
+                TitleView()
+                HStack {
+                    ButtonView(title: "Decrease", action: decreaseFontSize)
+                    ButtonView(title: "Increase", action: increaseFontSize)
+                }
+            }
+            .padding()
+        } else {
             HStack {
+                TitleView()
+
                 ButtonView(title: "Decrease", action: decreaseFontSize)
                 ButtonView(title: "Increase", action: increaseFontSize)
             }
+            .padding()
         }
-        .padding()
     }
 }
 
