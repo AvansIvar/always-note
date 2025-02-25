f64b93b01d73c5117db3125a777b0850b3744a43
AvansIvar <igm.schreutelkamp@student.avans.nl>
2025-02-25 12:37:50 +0100

    Initial Commit

---
+++ always-note/always_noteApp.swift
@@ -0,0 +1,17 @@
+//
+//  always_noteApp.swift
+//  always-note
+//
+//  Created by Ivar Schreutelkamp on 25/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct always_noteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ always-noteTests/always_noteTests.swift
@@ -0,0 +1,17 @@
+//
+//  always_noteTests.swift
+//  always-noteTests
+//
+//  Created by Ivar Schreutelkamp on 25/02/2025.
+//
+
+import Testing
+@testable import always_note
+
+struct always_noteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
